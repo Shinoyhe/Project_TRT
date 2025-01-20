e33ce06f3dfe0641fbf08f504f9761777d39b33b
tblagarrett <tblagarrett@gmail.com>
2025-01-20 12:29:11 -0800

    add card types
    you can also now get all cards of a type and sort the inventory by type

--- Assets/Scripts/CardData.cs
+++ Assets/Scripts/CardData.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class CardData : ScriptableObject
 {
     public string cardName;
+
+    [Tooltip("Valid Types: info, item")]
+    public string type;
+
     public string id;
     public string description;
     public Sprite sprite;

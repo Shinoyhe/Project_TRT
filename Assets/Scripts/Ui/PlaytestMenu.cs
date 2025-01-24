86a4faeda5d0701a026fb225bc2c82472fd098e2
Jonah Ryan <jonahrobot@gmail.com>
2025-01-24 14:34:12 -0800

    Add playtest menu controller
    In editor user can load all scenes in build path.
    
    Then in playMode, PlaytestMain.unity will display a button for each scene!
    
    To add:
    - Ability to open Playtest Menu across scenes.

---
+++ Assets/Scripts/Ui/PlaytestMenu.cs
@@ -0,0 +1,87 @@
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using System.IO;
+
+public class PlaytestMenu : MonoBehaviour {
+    
+    public GameObject SceneSelectButtonPrefab;
+    public GameObject ButtonParent;
+    public float DistanceBetweenButtons = 200;
+
+    public List<String> ScenesToCreate;
+
+    float _offset = 0;
+
+    private void Awake() {
+
+        int totalScenes = ScenesToCreate.Count;
+
+        for (int i = 0; i < totalScenes; i++) {
+            var scenePath = ScenesToCreate[i];
+
+            string sceneName = ObjectNames.NicifyVariableName(Path.GetFileNameWithoutExtension(scenePath));
+
+            CreateButton(sceneName, scenePath);
+        }
+    }
+
+    /// <summary>
+    /// Create a new scene Navigation Button 
+    /// </summary>
+    /// <param name="sceneName"> The name to display on Button. </param>
+    /// <param name="scenePath"> Path to scene in Asset Database.</param>
+    void CreateButton(string sceneName, string scenePath) {
+
+        // Create button
+        var button = Instantiate(SceneSelectButtonPrefab, Vector3.zero, Quaternion.identity, ButtonParent.transform);
+
+        // Align to position
+        button.GetComponent<RectTransform>().localPosition = new Vector3(0, _offset, 0);
+        _offset -= DistanceBetweenButtons;
+
+        // Set name
+        button.GetComponentInChildren<TMP_Text>().text = sceneName;
+
+        // Link Button to scene
+        button.GetComponent<Button>().onClick.AddListener(delegate () { SceneManager.LoadScene(scenePath); });
+    }
+}
+
+[CustomEditor(typeof(PlaytestMenu))]
+
+public class PlaytestMenuEditor : Editor {
+
+    /// <summary>
+    /// Init button to load all scenes in build path.
+    /// </summary>
+    public override void OnInspectorGUI() {
+
+        base.OnInspectorGUI();
+
+        // Add a custom button in the Inspector
+
+        if (GUILayout.Button("Add Scenes to Playtest Menu.")) {
+
+            PlaytestMenu playtestMenu = (PlaytestMenu)target;
+
+            int oldCount = playtestMenu.ScenesToCreate.Count;
+
+            playtestMenu.ScenesToCreate = new List<string>();
+
+            foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+                playtestMenu.ScenesToCreate.Add(scene.path);
+            }
+
+            Debug.Log("Added " + (playtestMenu.ScenesToCreate.Count - oldCount) + " to scene!");
+        }
+    }
+}

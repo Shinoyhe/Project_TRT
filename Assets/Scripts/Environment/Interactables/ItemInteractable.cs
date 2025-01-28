6e4aaefc31b3381214b1e3121b258f9857f9b088
TheGreatGreeble <73497579+TheGreatGreeble@users.noreply.github.com>
2025-01-27 16:06:22 -0800

    Interactable Detection working

---
+++ Assets/Scripts/Environment/Interactables/ItemInteractable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteractable : MonoBehaviour, IInteractable
+{
+  public void Interaction() {
+    throw new System.NotImplementedException();
+  }
+  public void Highlight() {
+    throw new System.NotImplementedException();
+  }
+  public void UnHighlight() {
+    throw new System.NotImplementedException();
+  }
+}

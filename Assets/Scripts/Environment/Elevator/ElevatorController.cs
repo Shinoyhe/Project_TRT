ea2413a07276066daebd5308ac93a42e3af9833d
tblagarrett <tblagarrett@gmail.com>
2025-02-10 12:22:41 -0800

    create elevator test scene

--- Assets/Scripts/Environment/Elevator/ElevatorController.cs
+++ Assets/Scripts/Environment/Elevator/ElevatorController.cs
@@ -6,6 +6,9 @@ using UnityEditor;
 [ExecuteAlways]
 public class ElevatorController : MonoBehaviour
 {
+
+    #region ========== [ Private Fields ] ==========
+
     [SerializeField] private List<Transform> waypoints = new List<Transform>();
 
     [Header("References")]
@@ -14,16 +17,23 @@ public class ElevatorController : MonoBehaviour
     [Space, Header("Setting the Platform")]
     [SerializeField] private Transform startingWaypoint;
 
-    private void OnValidate()
+    #endregion
+
+    void OnValidate()
     {
         if (waypoints.Count > 0 && startingWaypoint != null)
         {
-            SnapToWaypoint();
+            SnapObjectToWaypoint();
         }
     }
 
-    [Button("Snap to Waypoint")]
-    public void SnapToWaypoint()
+    #region ========== [ Public Methods ] ==========
+
+    /// <summary>
+    /// Snaps the position of Object in the elevator to the selected starting waypoint
+    /// </summary>
+    [SerializeField, Button("Snap to Waypoint")]
+    public void SnapObjectToWaypoint()
     {
         if (objectRoot != null)
         {
@@ -39,4 +49,27 @@ public class ElevatorController : MonoBehaviour
 
         }
     }
+
+    /// <summary>
+    /// Snaps the position of the selected starting waypoint to the Object in the elevator
+    /// </summary>
+    [SerializeField, Button("Snap Waypoint to Object")]
+    public void SnapWaypointToObject()
+    {
+        if (objectRoot != null)
+        {
+            #if UNITY_EDITOR
+            Undo.RecordObject(objectRoot, "Move waypoint");
+            #endif
+
+            startingWaypoint.position = objectRoot.position;
+
+            #if UNITY_EDITOR
+            EditorUtility.SetDirty(objectRoot);
+            #endif
+
+        }
+    }
+
+    #endregion
 }

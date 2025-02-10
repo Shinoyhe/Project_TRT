38a1eb38b9bbf52b3a933149208e850f2ae5e052
tblagarrett <tblagarrett@gmail.com>
2025-02-10 13:36:53 -0800

    Elevator moves player without trouble!

--- Assets/Scripts/Environment/Elevator/ElevatorController.cs
+++ Assets/Scripts/Environment/Elevator/ElevatorController.cs
@@ -2,37 +2,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
 using UnityEditor;
+using DG.Tweening;
 
 [ExecuteAlways]
 public class ElevatorController : MonoBehaviour
 {
 
-    #region ========== [ Private Fields ] ==========
+    #region ========== [ PARAMETERS ] ==========
 
     [SerializeField] private List<Transform> waypoints = new List<Transform>();
 
     [Header("References")]
     [SerializeField] private Transform objectRoot;
 
-    [Space, Header("Setting the Platform")]
-    [SerializeField] private Transform startingWaypoint;
+    [BoxGroup("Setup"), SerializeField] private Transform startingWaypoint;
+    [BoxGroup("Setup"), SerializeField, Range(0f, 10f)] private float movementDurationSeconds;
+    #endregion
+
+    #region ========== [ PRIVATE PROPERTIES ] ==========
+
+    private int startingWaypointIndex = -1;
+    private int nextIndexModifier = 1;
 
     #endregion
 
-    void OnValidate()
-    {
-        if (waypoints.Count > 0 && startingWaypoint != null)
-        {
-            SnapObjectToWaypoint();
-        }
-    }
-
-    #region ========== [ Public Methods ] ==========
+    #region ========== [ PUBLIC METHODS ] ==========
 
     /// <summary>
     /// Snaps the position of Object in the elevator to the selected starting waypoint
     /// </summary>
-    [SerializeField, Button("Snap to Waypoint")]
+    [SerializeField, Button("Snap Object to Waypoint")]
     public void SnapObjectToWaypoint()
     {
         if (objectRoot != null)
@@ -53,7 +52,6 @@ public class ElevatorController : MonoBehaviour
     /// <summary>
     /// Snaps the position of the selected starting waypoint to the Object in the elevator
     /// </summary>
-    [SerializeField, Button("Snap Waypoint to Object")]
     public void SnapWaypointToObject()
     {
         if (objectRoot != null)
@@ -71,5 +69,60 @@ public class ElevatorController : MonoBehaviour
         }
     }
 
+    #endregion
+
+    #region ========== [ PRIVATE METHODS ] ==========
+
+    private void SetStartingWaypointIndex()
+    {
+        if (!startingWaypoint) Debug.LogError("Elevator: Starting Waypoint not Assigned");
+
+        for (int i = 0; i < waypoints.Count; i++)
+        {
+            if (waypoints[i] == startingWaypoint)
+            {
+                startingWaypointIndex = i;
+                break;
+            }
+        }
+        if (startingWaypointIndex == -1)
+        {
+            Debug.LogError("Elevator: Could not get Starting Waypoint Index, waypoint is not in Waypoints list.");
+        }
+    }
+
+    [SerializeField, Button("Move Elevator")]
+    private void MoveElevator()
+    {
+        if ((startingWaypointIndex == waypoints.Count - 1 && nextIndexModifier > 0) || 
+            (startingWaypointIndex == 0 && nextIndexModifier < 0)) {
+            nextIndexModifier *= -1;
+        }
+        int targetWaypointIndex = startingWaypointIndex + nextIndexModifier;
+        Transform targetWaypoint = waypoints[targetWaypointIndex];
+    
+        objectRoot.transform.DOMove(targetWaypoint.position, movementDurationSeconds).SetEase(Ease.InOutQuad);
+        
+        startingWaypoint = targetWaypoint;
+        SetStartingWaypointIndex();
+    }
+    
+    void OnValidate()
+    {
+        if (waypoints.Count > 0 && startingWaypoint != null)
+        {
+            SnapObjectToWaypoint();
+        }
+    }
+
+    void Start()
+    {
+        SetStartingWaypointIndex();
+    }
+
+    void Update()
+    {
+        
+    }
     #endregion
 }

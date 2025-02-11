68bb807c6e6a5941485473b7d629250f98436476
Codebolt <44513207+codebolt7@users.noreply.github.com>
2025-02-11 02:56:16 -0800

    He tragically sacrificed himself to get more lines out of guilt

--- Assets/Scripts/Environment/Interactables/CardPickup.cs
+++ Assets/Scripts/Environment/Interactables/CardPickup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CardPickup : Interactable 
-{ 
+{
     [SerializeField] private InventoryCardData CardToGive;
 
     public override void Highlight() {

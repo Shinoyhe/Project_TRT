b11bd8021d026df8928bb71c7a2e8a190b2320e8
Codebolt <44513207+codebolt7@users.noreply.github.com>
2025-02-11 02:28:49 -0800

    Added regions to the script

--- Assets/Scripts/Environment/Interactables/InteractionIcon.cs
+++ Assets/Scripts/Environment/Interactables/InteractionIcon.cs
@@ -6,23 +6,30 @@ using DG.Tweening;
 
 public class InteractionIcon : MonoBehaviour
 {
-    [Header("General Parameters")]
-    [Tooltip("How fast the icon spins around."), SerializeField] private float rotationSpeed = 45;
-    [Tooltip("How fast the icon bobs up and down."), SerializeField] private float bobSpeed = 1;
-    [Tooltip("How much does the icon bob up and down."), SerializeField] private float bobIntensity = 0.1f;
+    #region ======== [ PARAMETERS ] ========
 
-    [Header("Tween Parameters")]
-    [Tooltip("Show animation duration"), SerializeField] private float showDuration = 0.25f;
-    [Tooltip("Show animation ease type"), SerializeField] private Ease showEase = Ease.OutBack;
-    [Tooltip("Hide animation duration"), SerializeField] private float hideDuration = 0.25f;
-    [Tooltip("Hide animation ease type"), SerializeField] private Ease hideEase = Ease.OutSine;
-    [Tooltip("For when the interaction icon is moving to a different point while visible."), SerializeField] private float moveSpeed = 10f;
+    [InfoBox("General Parameters"), Tooltip("How fast the icon spins around."), SerializeField] private float rotationSpeed = 45;
+    [InfoBox("General Parameters"), Tooltip("How fast the icon bobs up and down."), SerializeField] private float bobSpeed = 1;
+    [InfoBox("General Parameters"), Tooltip("How much does the icon bob up and down."), SerializeField] private float bobIntensity = 0.1f;
+
+    [InfoBox("Tween Parameters"), Tooltip("Show animation duration"), SerializeField] private float showDuration = 1f;
+    [InfoBox("Tween Parameters"), Tooltip("Show animation ease type"), SerializeField] private Ease showEase = Ease.OutElastic;
+    [InfoBox("Tween Parameters"), Tooltip("Hide animation duration"), SerializeField] private float hideDuration = 0.25f;
+    [InfoBox("Tween Parameters"), Tooltip("Hide animation ease type"), SerializeField] private Ease hideEase = Ease.OutSine;
+    [InfoBox("Tween Parameters"), Tooltip("For when the interaction icon is moving to a different point while visible."), SerializeField] private float moveSpeed = 10f;
+
+    #endregion
+
+    #region ======== [ PRIVATE PROPERTIES ] ========
 
     private Transform _targetTransfrom = null;
     private Vector3 _targetPosition = Vector3.zero;
     private Vector3 _position = Vector3.zero;
     private bool _active = false;
 
+    #endregion
+
+    #region ======== [ PUBLIC METHODS ] ========
 
     /// <summary>
     /// Shows the icon at the worldPosition
@@ -64,6 +71,9 @@ public class InteractionIcon : MonoBehaviour
         transform.DOScale(0, hideDuration).SetEase(hideEase);
     }
 
+    #endregion
+
+    #region ======== [ PRIVATE METHODS ] ========
 
     private void AnimateShow(float scale)
     {
@@ -110,12 +120,6 @@ public class InteractionIcon : MonoBehaviour
     }
 
 
-    void Start()
-    {
-
-    }
-
-
     void Update()
     {
         if (!IsVisible()) return;
@@ -126,4 +130,6 @@ public class InteractionIcon : MonoBehaviour
         // Update the target position if we have a target transform
         if (_targetTransfrom != null) _targetPosition = _targetTransfrom.position;
     }
+
+    #endregion
 }

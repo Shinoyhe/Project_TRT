420d26ae51f54ea0804107ae39ddcd68382ea4d9
Dylan Mahler <dmahler@ucsc.edu>
2025-02-10 17:22:12 -0800

    Baseline timeloop functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,39 @@
-using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class GameManager : Singleton<GameManager>
 {
+    // Public accessors ===========================================================================
+
     public static DialogueManager DialogueManager { get { return Instance.dialogueManager; } }
     public static PlayerInputHandler PlayerInput { get { return Instance.playerInput; } }
     public static UiInputHandler UiInput { get { return Instance.uiInput; } }
     public static Inventory Inventory { get { return Instance.inventory; } }
+    public static TimeLoopManager TimeLoopManager { get { return Instance.timeLoopManager; } }
     public static Player Player { get { return Instance.player; } }
 
+    // Backing fields =============================================================================
 
     [SerializeField] private DialogueManager dialogueManager;
     [SerializeField] private PlayerInputHandler playerInput;
     [SerializeField] private UiInputHandler uiInput;
     [SerializeField] private Inventory inventory;
+    [SerializeField] private TimeLoopManager timeLoopManager;
+    [SerializeField, Tag] private string playerTag;
     [SerializeField] private Player player;
+
+    // Initializers ===============================================================================
+
+    public void FindPlayer()
+    {
+        Debug.Log("FindPlayer called.");
+
+        GameObject playerParent = GameObject.FindWithTag(playerTag).transform.root.gameObject;
+        Debug.Log($"PlayerParent{(playerParent==null?" not":"")} found");
+
+        if (playerParent != null) {
+            player = playerParent.GetComponentInChildren<Player>();
+            Debug.Log($"Player{(player==null?" not":"")} found");
+        }
+    }
 }

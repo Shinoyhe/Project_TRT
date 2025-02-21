edb7f33792897920745ed40daa1d5b8d941f75ca
Jonah Ryan <jonahrobot@gmail.com>
2025-02-20 17:15:58 -0800

    Add all Ui States for In Game UI Controller

--- Assets/Scripts/Ui/InGameUi.cs
+++ Assets/Scripts/Ui/InGameUi.cs
@@ -1,82 +1,167 @@
+using UnityEditor.ShaderGraph;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InGameUi : MonoBehaviour
 {
     // Parameters =================================================================================
 
-    public Canvas HudCanvas;
-    public Canvas InventoryCanvas;
+    // Dialogue
+    [Header("Dependencies")]
+    public Canvas Default;
+    public Canvas Pause;
+    public Canvas Options;
+    public Canvas Controls;
+    public Canvas RebindControls;
+    public Canvas Journal;
+    public Canvas Inventory;
+    public Canvas Bartering;
 
-    private Canvas _currentCanvas;
-    private enum CanvasState {
-        HudCanvas,
-        InventoryCanvas
+    public enum UiStates {
+        Default,
+        Pause,
+        Options, 
+        MoveToTitle,
+        Controls,
+        RebindControls,
+        Journal, 
+        Inventory,
+        Bartering 
     }
-    private CanvasState _currentCanvasState;
+
+    private UiStates _currentCanvasState;
 
     // Initializers and Update ================================================================
-    void Start() 
-    {
-        if (HudCanvas == null || InventoryCanvas == null) {
-            Debug.LogError("Start UI not setup!");
+    void Start() {
+        if (Default == null) {
+            Debug.LogError("Default Canvas dependency not set.");
         }
 
-        _currentCanvasState = CanvasState.HudCanvas;
-        _currentCanvas = HudCanvas;
+        // Swap with Accessibility Check
+        MoveTo(UiStates.Default);
     }
-
     // Public Utility Methods ====================================================================
 
-    public void SwitchToHudScreen() 
-    {
-        SwitchCanvas(HudCanvas);
-        TimeLoopManager.SetLoopPaused(false);
-        _currentCanvasState = CanvasState.HudCanvas;
+    /// <summary>
+    /// Transition Start Ui to a new state.
+    /// </summary>
+    /// <param name="newState"> State to move to. </param>
+    public void MoveTo(UiStates newState) {
+
+        StopState(_currentCanvasState);
+        StartState(newState);
     }
 
-    public void SwitchToInventory() 
-    {
-        SwitchCanvas(InventoryCanvas);
-        TimeLoopManager.SetLoopPaused(true);
-        _currentCanvasState = CanvasState.InventoryCanvas;
+    // Used for button OnClick calls as they don't let enums to be passed through :|
+    public void MoveToDefault() => MoveTo(UiStates.Default);
+    public void MoveToPause() => MoveTo(UiStates.Pause);
+    public void MoveToOptions() => MoveTo(UiStates.Options);
+    public void MoveToTitle() => MoveTo(UiStates.MoveToTitle);
+    public void MoveToControls() => MoveTo(UiStates.Controls);
+    public void MoveToControlsRebind() => MoveTo(UiStates.RebindControls);
+    public void MoveToJournal() => MoveTo(UiStates.Journal);
+    public void MoveToInventory() => MoveTo(UiStates.Inventory);
+    public void MoveToBartering() => MoveTo(UiStates.Bartering);
+
+    public void Update() {
+    
     }
 
     // Private Helper Methods ====================================================================
 
-    /// <summary>
-    /// Switch from current canvas to a new canvas.
-    /// </summary>
-    /// <param name="canvas">New canvas to show.</param>
-    void SwitchCanvas(Canvas canvas) 
-    {
-        if (canvas == null) return;
 
-        if (_currentCanvas != null) {
-            _currentCanvas.gameObject.SetActive(false);
+    /// <summary>
+    /// Stop a currently running Ui state.
+    /// </summary>
+    /// <param name="stateToStop"> State that will stop. </param>
+    void StopState(UiStates stateToStop) {
+
+        // Can't stop transition states
+        // (MoveToTitle)
+
+        switch (stateToStop) {
+            case UiStates.Default:
+                // Insert animation!
+                Default.gameObject.SetActive(false);
+                break;
+            case UiStates.Pause:
+                // Insert animation!
+                Pause.gameObject.SetActive(false);
+                break;
+            case UiStates.Options:
+                // Insert animation!
+                Options.gameObject.SetActive(false);
+                break;
+            case UiStates.Controls:
+                // Insert animation!
+                Controls.gameObject.SetActive(false);
+                break;
+            case UiStates.RebindControls:
+                // Insert animation!
+                RebindControls.gameObject.SetActive(false);
+                break;
+            case UiStates.Journal:
+                // Insert animation!
+                Journal.gameObject.SetActive(false);
+                break;
+            case UiStates.Inventory:
+                // Insert animation!
+                Inventory.gameObject.SetActive(false);
+                break;
+            case UiStates.Bartering:
+                // Insert animation!
+                Bartering.gameObject.SetActive(false);
+                break;
         }
 
-        canvas.gameObject.SetActive(true);
-        _currentCanvas = canvas;
     }
 
-    // Update is called once per frame
-    void Update() 
-    {
-        if (GameManager.UiInput == null) return;
+    /// <summary>
+    /// Start a new state.
+    /// </summary>
+    /// <param name="stateToStart">State that will start.</param>
+    void StartState(UiStates stateToStart) {
 
-        // Keyboard input checking
-        if (_currentCanvasState == CanvasState.HudCanvas) {
-            if (GameManager.UiInput.GetSettingsDown()) {
-                SwitchToInventory();
-                return;
-            }
-        }
+        // Set our new state
+        _currentCanvasState = stateToStart;
 
-        if (_currentCanvasState != CanvasState.HudCanvas) {
-            if (GameManager.UiInput.GetSettingsDown()) {
-                SwitchToHudScreen();
-                return;
-            }
+        switch (stateToStart) {
+            case UiStates.Default:
+                // Insert animation!
+                Default.gameObject.SetActive(true);
+                break;
+            case UiStates.Pause:
+                // Insert animation!
+                Pause.gameObject.SetActive(true);
+                break;
+            case UiStates.Options:
+                // Insert animation!
+                Options.gameObject.SetActive(true);
+                break;
+            case UiStates.MoveToTitle:
+                // Insert animation!
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+                break;
+            case UiStates.Controls:
+                // Insert animation!
+                Controls.gameObject.SetActive(true);
+                break;
+            case UiStates.RebindControls:
+                // Insert animation!
+                RebindControls.gameObject.SetActive(true);
+                break;
+            case UiStates.Journal:
+                // Insert animation!
+                Journal.gameObject.SetActive(true);
+                break;
+            case UiStates.Inventory:
+                // Insert animation!
+                Inventory.gameObject.SetActive(true);
+                break;
+            case UiStates.Bartering:
+                // Insert animation!
+                Bartering.gameObject.SetActive(true);
+                break;
         }
     }
 }
--- Assets/Scripts/Ui/Start Ui.cs
+++ Assets/Scripts/Ui/Start Ui.cs
@@ -12,7 +12,7 @@ public class StartUi : MonoBehaviour {
     public Canvas AccessibilityCheck;
     public Canvas Controls;
 
-    public enum StartUiState {
+    public enum UiState {
         Title = 0,
         Credits = 1,
         Options = 2,
@@ -23,7 +23,7 @@ public class StartUi : MonoBehaviour {
         Controls = 7
     }
 
-    private StartUiState _currentCanvasState;
+    private UiState _currentCanvasState;
 
     // Initializers and Update ================================================================
     void Start() 
@@ -33,7 +33,7 @@ public class StartUi : MonoBehaviour {
         }
 
         // Swap with Accessibility Check
-        MoveTo(StartUiState.Title);
+        MoveTo(UiState.Title);
     }
 
     // Public Utility Methods ====================================================================
@@ -42,21 +42,21 @@ public class StartUi : MonoBehaviour {
     /// Transition Start Ui to a new state.
     /// </summary>
     /// <param name="newState"> State to move to. </param>
-    public void MoveTo(StartUiState newState) {
+    public void MoveTo(UiState newState) {
 
         StopState(_currentCanvasState);
         StartState(newState);
     }
 
     // Used for button OnClick calls as they don't let enums to be passed through :|
-    public void MoveToTitle() => MoveTo(StartUiState.Title);
-    public void MoveToCredits() => MoveTo(StartUiState.Credits);
-    public void MoveToOptions() => MoveTo(StartUiState.Options);
-    public void MoveToAccessibilityCheck() => MoveTo(StartUiState.AccessibilityCheck);
-    public void MoveToNewGame() => MoveTo(StartUiState.NewGame);
-    public void MoveToContinueGame() => MoveTo(StartUiState.ContinueGame);
-    public void MoveToQuit() => MoveTo(StartUiState.Quit);
-    public void MoveToControls() => MoveTo(StartUiState.Controls);
+    public void MoveToTitle() => MoveTo(UiState.Title);
+    public void MoveToCredits() => MoveTo(UiState.Credits);
+    public void MoveToOptions() => MoveTo(UiState.Options);
+    public void MoveToAccessibilityCheck() => MoveTo(UiState.AccessibilityCheck);
+    public void MoveToNewGame() => MoveTo(UiState.NewGame);
+    public void MoveToContinueGame() => MoveTo(UiState.ContinueGame);
+    public void MoveToQuit() => MoveTo(UiState.Quit);
+    public void MoveToControls() => MoveTo(UiState.Controls);
 
 
     // Private Helper Methods ====================================================================
@@ -66,29 +66,29 @@ public class StartUi : MonoBehaviour {
     /// Stop a currently running Ui state.
     /// </summary>
     /// <param name="stateToStop"> State that will stop. </param>
-    void StopState(StartUiState stateToStop) {
+    void StopState(UiState stateToStop) {
 
         // Can't stop transition states
         // (NewGame, ContinueGame, Quit)
 
         switch (stateToStop) {
-            case StartUiState.Title:
+            case UiState.Title:
                 // Insert animation!
                 TitleScreen.gameObject.SetActive(false);
                 break;
-            case StartUiState.Credits:
+            case UiState.Credits:
                 // Insert animation!
                 Credits.gameObject.SetActive(false);
                 break;
-            case StartUiState.Options:
+            case UiState.Options:
                 // Insert animation!
                 Options.gameObject.SetActive(false);
                 break;
-            case StartUiState.AccessibilityCheck:
+            case UiState.AccessibilityCheck:
                 // Insert animation!
                 AccessibilityCheck.gameObject.SetActive(false);
                 break;
-            case StartUiState.Controls:
+            case UiState.Controls:
                 // Insert animation!
                 Controls.gameObject.SetActive(false);
                 break;
@@ -100,47 +100,47 @@ public class StartUi : MonoBehaviour {
     /// Start a new state.
     /// </summary>
     /// <param name="stateToStart">State that will start.</param>
-    void StartState(StartUiState stateToStart) {
+    void StartState(UiState stateToStart) {
 
         // Set our new state
         _currentCanvasState = stateToStart;
 
         switch (stateToStart) {
-            case StartUiState.Title:
+            case UiState.Title:
                 // Insert animation!
                 TitleScreen.gameObject.SetActive(true);
                 break;
-            case StartUiState.Credits:
+            case UiState.Credits:
                 // Insert animation!
                 Credits.gameObject.SetActive(true);
                 break;
-            case StartUiState.Options:
+            case UiState.Options:
                 // Insert animation!
                 Options.gameObject.SetActive(true);
                 break;
-            case StartUiState.NewGame:
+            case UiState.NewGame:
                 // Start new game
                 // -- TEMP --
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
                 // -- TEMP --
                 break;
-            case StartUiState.ContinueGame:
+            case UiState.ContinueGame:
                 // Continue game
                 // -- TEMP --
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
                 // -- TEMP --
                 break;
-            case StartUiState.Quit:
+            case UiState.Quit:
                 // Save then quit
                 // -- TEMP -- 
                 Application.Quit();
                 // -- TEMP --
                 break;
-            case StartUiState.AccessibilityCheck:
+            case UiState.AccessibilityCheck:
                 // Insert animation!
                 AccessibilityCheck.gameObject.SetActive(true);
                 break;
-            case StartUiState.Controls:
+            case UiState.Controls:
                 // Insert animation!
                 Controls.gameObject.SetActive(true);
                 break;

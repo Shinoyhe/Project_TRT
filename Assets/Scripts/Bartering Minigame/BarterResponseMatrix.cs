7a0969097063bb2fc27f3afb98d96cd1012a8148
Dylan Mahler <dmahler@ucsc.edu>
2025-02-22 11:22:15 -0800

    (WIP, Stable) Barter Matrices now
    TODO: Clean up BarterReponseMatrix
    TODO: Refactor scoring to use matrices

--- Assets/Scripts/Bartering Minigame/BarterResponseMatrix.cs
+++ Assets/Scripts/Bartering Minigame/BarterResponseMatrix.cs
@@ -14,24 +14,62 @@ public class BarterResponseMatrix : ScriptableObject
     public enum State {
         POSITIVE,
         NEUTRAL,
-        NEGATIVE
+        NEGATIVE,
     }
-
-    [SerializeField, Tooltip("The cards that this opponent can play.")]
-    private PlayingCard[] oppCards = new PlayingCard[0];
-    public PlayingCard[] OppCards => oppCards;
 
     [Tooltip("An ID used for debug purposes.")]
     public string id;
+    [SerializeField, Tooltip("The cards that this opponent can play.")]
+    private PlayingCard[] oppCards = new PlayingCard[0];
+    public PlayingCard[] OppCards => oppCards;
     [Tooltip("The default state returned whenever the player plays a card not in OppCards.")]
     public State defaultState = State.NEUTRAL;
-    public Dictionary<PlayingCard, Dictionary<PlayingCard, State>> oppPlayerToMatch;
 
-    // Initializers ===============================================================================
+    [System.Serializable]
+    public class CardCardStateTriplet {
+        public PlayingCard OppCard;
+        public PlayingCard PlayerCard;
+        public State Match;
+
+        public CardCardStateTriplet(PlayingCard oppCard, PlayingCard playerCard, State match)
+        {
+            OppCard = oppCard;
+            PlayerCard = playerCard;
+            Match = match;
+        }
+    }
+
+    [SerializeField] private List<CardCardStateTriplet> editorMatchList;
+    public Dictionary<PlayingCard, Dictionary<PlayingCard, State>> runtimeMatchDict;
+
+    // Editor-time methods ========================================================================
 
     private void OnEnable()
     {
-        UpdateOppCards();
+        UpdateTriplets();
+    }
+
+    // Runtime methods ============================================================================
+
+    /// <summary>
+    /// Called once at runtime, before this matrix is ever read from. Loads our triplet list into
+    /// our runtime dictionary.
+    /// </summary>
+    public void InitializeDict()
+    {
+        if (runtimeMatchDict == null) {
+            runtimeMatchDict = new();
+        } else {
+            runtimeMatchDict.Clear();
+        }
+
+        foreach (var triplet in editorMatchList) {
+            if (!runtimeMatchDict.ContainsKey(triplet.OppCard)) {
+                runtimeMatchDict[triplet.OppCard] = new();
+            }
+
+            runtimeMatchDict[triplet.OppCard][triplet.PlayerCard] = triplet.Match;
+        }
     }
 
     /// <summary>
@@ -45,207 +83,207 @@ public class BarterResponseMatrix : ScriptableObject
         if (!oppCards.Contains(oppCard)) {
             Debug.LogError($"BarterResponseMatrix Error: GetMatch failed. Supplied opponent card "
                          + $"(id {oppCard}) was not in oppPlayerToMatch dictionary.");
+            return State.NEUTRAL;
         }
 
-        if (!oppPlayerToMatch[oppCard].ContainsKey(playerCard)) {
+        if (!runtimeMatchDict[oppCard].ContainsKey(playerCard)) {
             return defaultState;
         }
 
         // Else, our indexing is valid!
-        return oppPlayerToMatch[oppCard][playerCard];
+        return runtimeMatchDict[oppCard][playerCard];
     }
 
-    // Editor-friendly methods ====================================================================
+    // Editor-friend methods ======================================================================
 
-    private void UpdateOppCards()
+    private void UpdateTriplets()
     {
-        oppPlayerToMatch ??= new();
+        // Updates our dictionary based on the contents of our OppCards list.
+        // ================
+
+        // Lazily initialize- if null, create.
+        // editorMatchList ??= new();
 
         // Remove missing cards.
-        foreach (PlayingCard oldCard in oppPlayerToMatch.Keys.ToList()) {
-            if (!oppCards.Contains(oldCard)) {
-                // Ignore this card when played by the opp...
-                oppPlayerToMatch.Remove(oldCard);
-                // And ignore this card when played by the player...
-                foreach (PlayingCard remainingCard in oppPlayerToMatch.Keys) {
-                    oppPlayerToMatch[remainingCard].Remove(oldCard);
-                }
+        // Iterate through an array copy, so we can remove from the list without getting changed-
+        // while-iterating errors.
+        foreach (var triplet in editorMatchList.ToArray()) {
+            // If one of the triplet's cards isn't in our valid list, remove the triplet.
+            if (!OppCards.Contains(triplet.OppCard) || !OppCards.Contains(triplet.PlayerCard)) {
+                editorMatchList.Remove(triplet);
             }
         }
 
         // Add new cards.
-        foreach (PlayingCard newCard in oppCards) {
-            // If the card is new, initialize it.
-            if (!oppPlayerToMatch.ContainsKey(newCard)) {
-                // Pay attention when this card is played by the opp...
-                oppPlayerToMatch[newCard] = new();
-                // And when this card is played by the player.
-                foreach (PlayingCard existingCard in oppPlayerToMatch.Keys) {
-                    oppPlayerToMatch[existingCard][newCard] = State.NEUTRAL;
-
-                    if (!newCard.Matches(existingCard)) {
-                        oppPlayerToMatch[newCard][existingCard] = State.NEUTRAL;
+        foreach (PlayingCard oppCard in OppCards) {
+            // If we don't have any triplets with our oppCard, instantiate them.
+            if (!editorMatchList.Any(x => x.OppCard == oppCard || x.PlayerCard == oppCard)) {
+                foreach (PlayingCard playerCard in OppCards) {
+                    // Add the opponent-player pair...
+                    editorMatchList.Add(new(oppCard, playerCard, State.NEUTRAL));
+                    // and the player-opponent pair (if they're not the same)
+                    if (oppCard != playerCard) {
+                        editorMatchList.Add(new(playerCard, oppCard, State.NEUTRAL));
                     }
                 }
             }
         }
+        
     }
 
-    private void SetMatch(PlayingCard oppCard, PlayingCard playerCard, State match)
+    private void SetMatchEditor(PlayingCard oppCard, PlayingCard playerCard, State match)
     {
-        oppPlayerToMatch[oppCard][playerCard] = match;
+        var triplet = editorMatchList.Find(x => x.OppCard == oppCard && x.PlayerCard == playerCard);
+        triplet.Match = match;
     }
 
-    private void RemoveDuplicateOppCards()
+    private State GetMatchEditor(PlayingCard oppCard, PlayingCard playerCard)
     {
+        var triplet = editorMatchList.Find(x => x.OppCard == oppCard && 
+                                           x.PlayerCard == playerCard);
+        return triplet.Match;
+    }
+
+    private void CleanUpOppCards()
+    {
+        // Remove duplicates and null cards.
         oppCards = oppCards.Distinct().Where(x => x != null).ToArray();
     }
 
-    // Editor class ===============================================================================
+    #region Editor class ==========================================================================
 
-    #if UNITY_EDITOR
+    #if UNITY_EDITOR 
+    /// <summary>
+    /// Script enclosing a custom editor for instances of the BarterResponseMatrix class.
+    /// 
+    /// The primary addition is a grid of enums that allows the bidimensional Card-Card-State
+    /// dictionary to be set in the inspector!
+    /// </summary>
     [CustomEditor(typeof(BarterResponseMatrix))]
     public class EditorSpawnTemplate : Editor
     {
+        // The object that this editor applies to.
         private BarterResponseMatrix _matrix;
-        // Check, empty character, or cross
+
+        // The three options for the enum checkbox- check, empty character, or cross.
         private static readonly string[] _stateOpts = new []{"✓", "‎", "✗"};
+        // How many characters we trim IDs to, used for column/row headers.
         private static readonly uint _idLength = 3;
+        // A GUIStyle used to make horizontal lines.
+        private static readonly GUIStyle _horizontalLine = new();
+
+        // An int array that holds the state of all the dropdowns.
         private int[,] _choiceIndex;
+        // As of the last time we checked, the length of _matrix.OppCards
         private int _lastCount;
-        
-        private string[] _oppCardNames;
+        // Where we store the IDs of cards, used to print headers.
+        private string[] _cardIds;
+
+        // Initializers ===========================================================================
 
         private void OnEnable()
         {
             _matrix = (BarterResponseMatrix)target;
-
-            _matrix.RemoveDuplicateOppCards();
-            _matrix.UpdateOppCards();
-
-            _lastCount = _matrix.OppCards.Length;
-
-            RegenerateTableHeaders();
-
-            _choiceIndex = new int[_lastCount,_lastCount];
-            for (int r=0; r < _lastCount; r++) {
-                for (int c=0; c < _lastCount; c++) {
-                    PlayingCard oppCard = _matrix.OppCards[c];
-                    PlayingCard playerCard = _matrix.OppCards[r];
-                    _choiceIndex[r,c] = (int)_matrix.GetMatch(oppCard, playerCard);
-                }
-            }
+            OppCardsChanged();
         }
 
+        private void OnDisable()
+        {
+            OppCardsChanged();
+        }
+
+        // Primary Methods ========================================================================
+
+        /// <summary>
+        /// Overrides the default Inspector editor behavior.
+        /// 
+        /// The primary addition is a grid of enums that allows the bidimensional Card-Card-State
+        /// dictionary to be set in the inspector!
+        /// </summary>
         public override void OnInspectorGUI()
         {
-            if (!_matrix) return;
-
             serializedObject.Update();
 
-            if (_matrix.OppCards.Length < _lastCount) {
-                UpdateMatrix();
-            }
+            Multispace(3);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("id"));
+            Multispace(3);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("oppCards"));
 
-            if (GUILayout.Button("Update OppCards in Matrix")) {
-                UpdateMatrix();
-            }
+            if (_matrix) {
 
-            EditorGUILayout.BeginHorizontal();
-                EditorGUIUtility.labelWidth = 1;
-                EditorGUILayout.LabelField("Opponent >", EditorStyles.miniLabel);
-                for (int c = 0; c < _lastCount; c++) {
-                    EditorGUILayout.LabelField(GetShortId(c), EditorStyles.boldLabel);
-                }
-            EditorGUILayout.EndHorizontal();
+                if (_matrix.OppCards.Length < _lastCount) OppCardsChanged();
+                if (GUILayout.Button("Update OppCards in Matrix")) OppCardsChanged();
 
-            EditorGUILayout.LabelField("Player v", EditorStyles.miniLabel);
+                Multispace(3);
+                HorizontalLine(Color.grey);
+                Multispace(3);
 
-            for (int r = 0; r < _lastCount; r++) {
+                // Write column labels
                 EditorGUILayout.BeginHorizontal();
-
                     EditorGUIUtility.labelWidth = 1;
-                    EditorGUILayout.LabelField(GetShortId(r), EditorStyles.boldLabel);
-
+                    EditorGUILayout.LabelField("Opponent >", EditorStyles.miniLabel);
                     for (int c = 0; c < _lastCount; c++) {
-
-                        Color color = (State)_choiceIndex[r,c] switch {
-                            State.POSITIVE => Color.green,
-                            State.NEGATIVE => Color.red,
-                            _ => Color.white
-                        };
-
-                        GUI.backgroundColor = color;
-                        int newIndex = EditorGUILayout.Popup(_choiceIndex[r,c], _stateOpts);
-                        // If changed, write back an update.
-                        if (newIndex != _choiceIndex[r,c]) {
-                            _choiceIndex[r,c] = newIndex;
-                            _matrix.SetMatch(_matrix.OppCards[c], _matrix.OppCards[r], 
-                                            (State)newIndex);
-                        }   
+                        EditorGUILayout.LabelField(GetShortId(c), EditorStyles.boldLabel);
                     }
-
                 EditorGUILayout.EndHorizontal();
-                GUI.backgroundColor = Color.white;
-            }
 
-            // Reset label width and GUI color.
-            EditorGUIUtility.labelWidth = 0;
-            GUI.backgroundColor = Color.white;
+                EditorGUILayout.LabelField("Player v", EditorStyles.miniLabel);
 
-            serializedObject.ApplyModifiedProperties();
+                // Write rows
+                for (int r = 0; r < _lastCount; r++) {
+                    EditorGUILayout.BeginHorizontal();
+                        // Write row labels
+                        EditorGUIUtility.labelWidth = 1;
+                        EditorGUILayout.LabelField(GetShortId(r), EditorStyles.boldLabel);
+                        // Write columns of enum checkboxes
+                        for (int c = 0; c < _lastCount; c++) {
 
-            base.OnInspectorGUI();
+                            GUI.backgroundColor = (State)_choiceIndex[r,c] switch {
+                                State.POSITIVE => Color.green,
+                                State.NEGATIVE => Color.red,
+                                _ => Color.white
+                            };
 
-            // ================
-            // Debug testing
-            // ================
-
-            // MiscEditorMethods.HorizontalLine(Color.white);
-
-            EditorGUILayout.BeginHorizontal();
-                EditorGUIUtility.labelWidth = 1;
-                EditorGUILayout.LabelField("Opponent >", EditorStyles.miniLabel);
-                for (int c = 0; c < _lastCount; c++) {
-                    EditorGUILayout.LabelField(GetShortId(c), EditorStyles.boldLabel);
-                }
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.LabelField("Player v", EditorStyles.miniLabel);
-
-            for (int r = 0; r < _lastCount; r++) {
-                EditorGUILayout.BeginHorizontal();
-
-                    EditorGUIUtility.labelWidth = 1;
-                    EditorGUILayout.LabelField(GetShortId(r), EditorStyles.boldLabel);
-
-                    for (int c = 0; c < _lastCount; c++) {
-
-                        Color color = (State)_choiceIndex[r,c] switch {
-                            State.POSITIVE => Color.green,
-                            State.NEGATIVE => Color.red,
-                            _ => Color.white
-                        };
-
-                        if (GUILayout.Button("Test")) {
-                            Debug.Log($"Opp({_matrix.OppCards[c]}) Player({_matrix.OppCards[r]}) "
-                                    + $"= {_matrix.GetMatch(_matrix.OppCards[c], _matrix.OppCards[r])}");
+                            int newIndex = EditorGUILayout.Popup(_choiceIndex[r,c], _stateOpts);
+                            // If changed, write back an update.
+                            if (newIndex != _choiceIndex[r,c]) {
+                                _choiceIndex[r,c] = newIndex;
+                                _matrix.SetMatchEditor(_matrix.OppCards[c], _matrix.OppCards[r], 
+                                                (State)newIndex);
+                                
+                                // If we changed one of the matchups, make sure we save to disk.
+                                EditorUtility.SetDirty(_matrix);
+                            }
                         }
-                    }
 
-                EditorGUILayout.EndHorizontal();
+                    EditorGUILayout.EndHorizontal();
+                    // Reset the color after setting it for the enum checkbox.
+                    GUI.backgroundColor = Color.white;
+                }
+
+                // Reset label width and GUI color.
+                EditorGUIUtility.labelWidth = 0;
                 GUI.backgroundColor = Color.white;
             }
 
-            // Reset label width and GUI color.
-            EditorGUIUtility.labelWidth = 0;
-            GUI.backgroundColor = Color.white;
+            Multispace(3);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("defaultState"));
+
+            // Save our changes to the asset.
+            serializedObject.ApplyModifiedProperties();
         }
 
-        private void UpdateMatrix()
+        private void OppCardsChanged()
         {
-            _matrix.RemoveDuplicateOppCards();
-            _matrix.UpdateOppCards();
+            // Called when OppCards changes (either automatically when it shrinks, or manually
+            // when growing or permutating).
+            // 
+            // Validates the OppCards list, updates our triplet list to just have valid entries,
+            // repaints our _choiceIndex array, and rewrites our headers.
+            // ================
+
+            _matrix.CleanUpOppCards();
+            _matrix.UpdateTriplets();
 
             _lastCount = _matrix.OppCards.Length;
             _choiceIndex = new int[_lastCount, _lastCount];
@@ -253,32 +291,54 @@ public class BarterResponseMatrix : ScriptableObject
                 for (int c = 0; c < _lastCount; c++) {
                     PlayingCard oppCard = _matrix.OppCards[c];
                     PlayingCard playerCard = _matrix.OppCards[r];
-                    _choiceIndex[r, c] = (int)_matrix.GetMatch(oppCard, playerCard);
+
+                    _choiceIndex[r,c] = (int)_matrix.GetMatchEditor(oppCard, playerCard);
                 }
             }
 
-            RegenerateTableHeaders();
+            RegenerateCardIds();
         }
 
-        private void RegenerateTableHeaders()
+        private void RegenerateCardIds()
         {
-            _oppCardNames = _matrix.OppCards.Select(x => (x != null) ? x.Id : "Null").ToArray();
+            _cardIds = _matrix.OppCards.Select(x => (x != null) ? x.Id : "Null").ToArray();
         }
+
+        //  Visual helpers ========================================================================
 
         private string GetShortId(int i)
         {
-            if (i < 0 || i >= _oppCardNames.Length) {
+            if (i < 0 || i >= _cardIds.Length) {
                 Debug.LogError($"BarterResponseMatrix Editor Error: GetShortId failed. Index ({i}) was "
-                            + $"out of range of _oppCardNames (length {_oppCardNames.Length}).");
+                            + $"out of range of _oppCardNames (length {_cardIds.Length}).");
                 return "Null";
             }
 
             // Take our abbreviated length, or the full id length, whichever is shorter.
-            string id = _oppCardNames[i];
+            string id = _cardIds[i];
             int length = (int)Mathf.Min(_idLength, id.Length);
 
             return id[0..length];
         }
+
+        private static void HorizontalLine(Color color) 
+        {
+            _horizontalLine.normal.background = EditorGUIUtility.whiteTexture;
+            _horizontalLine.margin = new RectOffset(0, 0, 4, 4);
+            _horizontalLine.fixedHeight = 1;
+
+            var oldColor = GUI.color;
+            GUI.color = color;
+            GUILayout.Box(GUIContent.none, _horizontalLine);
+            GUI.color = oldColor;
+        }
+
+        private static void Multispace(int times)
+        {
+            for (int i = 0; i < times; i++) EditorGUILayout.Space();
+        }
     }
     #endif
+
+    #endregion
 }

f07808c521b1db9e849f974014885385be78e720
tblagarrett <tblagarrett@gmail.com>
2025-03-06 13:36:13 -0800

    Hide Interact Icon when LockedInteractable is Locked

--- Assets/Scripts/Environment/Interactables/LockedInteractable.cs
+++ Assets/Scripts/Environment/Interactables/LockedInteractable.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,6 +7,15 @@ public class LockedInteractable : Interactable
     [SerializeField] private bool removeIfItem = false;
     [SerializeField] private bool locked = true;
     [SerializeField] private UnityEvent callbackFunction;
+
+    private Vector3 _iconPositionStorage;
+    private bool _useTransformStorage;
+
+    private void Start()
+    {
+        _iconPositionStorage = IconLocalPosition;
+        _useTransformStorage = UseTransform;
+    }
 
     public override void Interaction()
     {
@@ -31,11 +37,32 @@ public class LockedInteractable : Interactable
 
     public override void Highlight()
     {
+        if (!GameManager.Instance) { return; }
 
+        if (!locked || GameManager.Inventory.HasCard(RequiredCard))
+        {
+            UseTransform = _useTransformStorage;
+            IconLocalPosition = _iconPositionStorage;
+        } else if (locked || !GameManager.Inventory.HasCard(RequiredCard))
+        {
+            UseTransform = false;
+            IconLocalPosition = Vector3.down * 100;
+        }
     }
 
     public override void UnHighlight()
     {
-        
+        if (!GameManager.Instance) { return; }
+
+        if (!locked || GameManager.Inventory.HasCard(RequiredCard))
+        {
+            UseTransform = _useTransformStorage;
+            IconLocalPosition = _iconPositionStorage;
+        }
+        else if (locked || !GameManager.Inventory.HasCard(RequiredCard))
+        {
+            UseTransform = false;
+            IconLocalPosition = Vector3.down * 100;
+        }
     }
 }

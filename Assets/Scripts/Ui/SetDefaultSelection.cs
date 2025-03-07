ce7f92a0349b91e5667eba03af9c4ebdd7dc79c9
Jonah Ryan <jonahrobot@gmail.com>
2025-03-07 01:14:11 -0800

    More Small Changes

--- Assets/Scripts/Ui/SetDefaultSelection.cs
+++ Assets/Scripts/Ui/SetDefaultSelection.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class SetDefaultSelection : MonoBehaviour
 {
     public Button DefaultButton;
+    public bool TriggerOnClick = false;
 
     private void OnEnable() {
 
         if (DefaultButton != null) {
             DefaultButton.Select();
-
+            if (TriggerOnClick) {
+                DefaultButton.onClick.Invoke();
+            }
             var buttonHandler = DefaultButton.gameObject.GetComponent<BoldTextOnHover>();
             if (buttonHandler) {
                 buttonHandler.TriggerBold();

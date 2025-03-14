7bf15816069c784bed5bafce0c88c5c905613c32
sweetautumndeer <amoulios@yahoo.com>
2025-03-13 19:44:44 -0700

    Re-added Settings UI SFX in Wwise

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -19,6 +19,7 @@ namespace AK
         static const AkUniqueID BART_CARD_PLACE = 3630843069U;
         static const AkUniqueID BART_CARD_SELECT = 349408092U;
         static const AkUniqueID BART_CARD_SHUFFLE = 362751767U;
+        static const AkUniqueID BIRDSTART = 3943065624U;
         static const AkUniqueID DOOROPEN = 1404805401U;
         static const AkUniqueID ELEVATORHUM = 1344299683U;
         static const AkUniqueID ELEVATORSTARTUP = 1220165926U;
@@ -32,10 +33,19 @@ namespace AK
         static const AkUniqueID MUSICSTATECALM = 821441420U;
         static const AkUniqueID MUSICSTATEPLAYERSPAWN = 1031159975U;
         static const AkUniqueID PAUSEMUSIC = 1127073398U;
+        static const AkUniqueID PLAY_MACHINEHUM1_LOOPCONTAINER = 30480848U;
+        static const AkUniqueID PLAY_MACHINEHUM2_LOOPCONTAINER = 1929638735U;
+        static const AkUniqueID PLAY_MENU_BUTTONDOWN = 1536324566U;
+        static const AkUniqueID PLAY_MENU_BUTTONUP = 1033222429U;
+        static const AkUniqueID PLAY_MENU_CLOSE = 1357484874U;
+        static const AkUniqueID PLAY_MENU_OPEN = 2236734574U;
+        static const AkUniqueID PLAY_WATERFALL_BASE_LOOPCONTAINER = 2874767734U;
+        static const AkUniqueID PLAY_WATERFALL_CLOSESPARKLE_LOOPCONTAINER = 3296751295U;
         static const AkUniqueID PLAYERFOOTSTEPS = 1681012287U;
         static const AkUniqueID RESUMEMUSIC = 2170724709U;
         static const AkUniqueID STARTMUSIC = 3827058668U;
         static const AkUniqueID STOPMUSIC = 1917263390U;
+        static const AkUniqueID TESTAMBIENCESTART = 664772623U;
         static const AkUniqueID UI_MENU_CLOSE = 3226991506U;
         static const AkUniqueID UI_MENU_OPEN = 4083126854U;
     } // namespace EVENTS
@@ -75,6 +85,7 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENCE = 85412153U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
     } // namespace BANKS
@@ -84,8 +95,15 @@ namespace AK
         static const AkUniqueID AMBIENCE_BUS = 4202237879U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID REVERBS = 3545700988U;
         static const AkUniqueID SFX_BUS = 1502772432U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CHURCH = 2568407992U;
+        static const AkUniqueID OUTDOORS = 2730119150U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
